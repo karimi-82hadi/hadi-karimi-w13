6ba7356e654bd734c9adc92bf0dce5cc61fce0c7
Hadi Karimi <karimi1382hadi@gmail.com>
2024-08-21 22:30:33 +0330

    set styles to TabsComponent

--- src/components/TabComponent.jsx
+++ src/components/TabComponent.jsx
@@ -1,7 +1,28 @@
 import styles from "./TabComponent.module.css";
 
 function TabComponent() {
-  return <div>TabComponent</div>;
+  return (
+    <div className={styles.tabContainer}>
+      <div className={styles.tabHeader}>
+        <h1>Tabs Component with React</h1>
+        <ul>
+          <li className={styles.active}>Tab 1</li>
+          <li>Tab 2</li>
+          <li>Tab 3</li>
+          <li>Tab 4</li>
+        </ul>
+      </div>
+      <div className={styles.tabContent}>
+        <h2>Content 1</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe sit
+          quisquam explicabo eaque iure aliquid, culpa veniam perferendis vero
+          maxime reiciendis ab? Modi laboriosam rerum quos illo amet itaque
+          culpa?
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default TabComponent;
